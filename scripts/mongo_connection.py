60ca1deda9f20f17cb9a8152673daf86564bc71c
Antoine MOHR <antoine.mohr@viacesi.fr>
2020-06-23 19:15:40 +0200

    Add multiple stats

--- scripts/mongo_connection.py
+++ scripts/mongo_connection.py
@@ -10,18 +10,32 @@ client = MongoClient('localhost', 27017)
 db = client["DataProject"]
 stats = db["stats"]
 
-
-def insert_stats(x,y, name, x_label="", y_label=""):
+def insert_stats(x,y, name, x_label="", y_label="", specification="", dataset_name=""):
     dict_stats_points = {
         'name':name,
         'x':x,
         'y':y,
         "x_label":x_label,
         "y_label":y_label,
+        "specification":specification,
+        "dataset_name":dataset_name,
         
     }
     
     stats.insert_one(dict_stats_points)
+
+
+
+def insert_multiple_stats(x_array, y_array, name, x_label, y_label, specification_array, dataset_name_array):
+    
+    if(len(x_array) != len(y_array)):
+        raise Exception('Len X != Len Y !')
+    
+    length = len(x_array);
+    for i in range(length):
+        insert_stats(x_array[i], y_array[i], name, x_label, y_label, specification_array[i],dataset_name_array[i] )
+
+
 
 
 def get_stats(name):
@@ -42,9 +56,11 @@ def show_stats(name):
     plt.plot(x, y)
     plt.show()
 
-'''
+"""
 if __name__ == '__main__':
     insert_stats([1,2,3,4,5],[4,8,7,54,4], 'test','s','exec')
+    
+    insert_multiple_stats([[1,2,3],[1,2,3]], [[10,15,25],[3,6,9]],"temps en fct nb solutions", "nb solutions","temps", ['tabu','simulated'],['n32-k5','n32-k5'])
     print(get_stats('test')[0])
     show_stats("test")
-'''
+"""
