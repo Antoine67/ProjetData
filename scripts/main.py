61c0b572e81fbd354984a940e6431d3f5e24fd52
Antoine MOHR <antoine.mohr@viacesi.fr>
2020-06-25 09:55:32 +0200

    Removed

--- scripts/main.py
+++ scripts/main.py
@@ -45,13 +45,7 @@ algos_metaheuristic = [
 
 def main():
     
-    display_statistics([{'name':'Temps execution en fonction des solutions',
-                         'specification':3,
-                         'dataset_name':'A-n33-k6'},
-                        {'name':'Temps execution en fonction des solutions',
-                         'specification':4,
-                         'dataset_name':'A-n33-k6'}])
-    return
+
     displayMenu()
     return
     
